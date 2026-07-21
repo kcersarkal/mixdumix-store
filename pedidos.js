cdf4ee7d7c1d5d32d4ca76cf99509b4c6616b94c
kcersarkal <kcersar.kal@gmail.com>
2026-07-21 14:16:11 -0300

    Novo pedido via n8n

--- pedidos.js
+++ pedidos.js
@@ -52,5 +52,14 @@ window.PEDIDOS = [
     "preco": 979.9,
     "link": "https://kcersarkal.github.io/MixDuMix.of/?pedido=B07GWXBGYS",
     "descricao": "Mesa em aço inox escovado. 1 tripla Chama (3700W), 2 rápido (2900W) e 2 semi-rápido (1750W). Queimadores italianos. Acendimento super automático. Trempes dupla e individual em ferro fundido com acabamento fosco."
+  },
+  {
+    "id": "B0FLGD78Z4",
+    "asin": "B0FLGD78Z4",
+    "titulo": "Samsung Smart TV 50&#34; Crystal UHD 4K U8600F 2025 + Soundbar HW-B400F/ZD",
+    "imagem": "https://m.media-amazon.com/images/I/51MIeqZCK8L._AC_SX679_.jpg",
+    "preco": 2479.86,
+    "link": "https://www.amazon.com.br/dp/B0FLGD78Z4?tag=mixdm22-20&ref_=mixdumix_pedido",
+    "descricao": "Samsung Smart TV 50&#34; Crystal UHD 4K U8600F 2025 + Soundbar HW-B400F/ZD"
   }
 ];
