534b2ff3e513e0d949e9014c3a743a94d86c40ef
Mix Du Mix Ofertas <mix.dumixof@gmail.com>
2026-08-03 11:00:34 -0300

    Atualizacao automatica de precos: pedidos.js

--- pedidos.js
+++ pedidos.js
@@ -4,7 +4,7 @@ window.PEDIDOS = [
     "asin": "B0FLGD78Z4",
     "titulo": "Samsung Smart TV 50&#34; Crystal UHD 4K U8600F 2025 + Soundbar HW-B400F/ZD",
     "imagem": "https://m.media-amazon.com/images/I/51MIeqZCK8L._AC_SX679_.jpg",
-    "preco": 2584.0,
+    "preco": 2474.76,
     "link": "https://www.amazon.com.br/dp/B0FLGD78Z4?tag=mixstore04b3-20&ref_=mixdumix_pedido",
     "descricao": "Samsung Smart TV 50&#34; Crystal UHD 4K U8600F 2025 + Soundbar HW-B400F/ZD"
   },
