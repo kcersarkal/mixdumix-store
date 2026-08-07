fcf10d0bfaa14f8c120bd3412439b48f240b08b7
kcersarkal <kcersar.kal@gmail.com>
2026-08-07 08:57:28 -0300

    Novo pedido via n8n

--- pedidos.js
+++ pedidos.js
@@ -97,5 +97,12 @@ window.PEDIDOS = [
     "preco": 63.78,
     "link": "https://www.amazon.com.br/dp/B0F89VV1YS?tag=mixstore04b3-20&ref_=mixdumix_pedido",
     "descricao": "MATERIAIS DE QUALIDADE: Essa chaleira de metal é feita de aço inoxidável 304 de alta qualidade, durável, atóxico, sem BPA e inodoro, garantindo que suas bebidas sejam seguras e saudáveis.. ISOLAMENTO A VÁCUO: A Thermo Kettle apresenta isolamento de aço inoxidável de camada dupla que mantém as bebidas frias por até 24 horas e quentes por até 12 horas, garantindo que você possa desfrutar de suas bebidas favoritas na temperatura perfeita durante todo o dia.. CINTA DE LONA: Essa garrafa de água de aço inoxidável vem com um cordão para que você possa carregá-la com conforto e estilo. Perfeita para acampamentos, caminhadas, ciclismo e montanhismo.. FÁCIL DE USAR: Garrafa de água grande com boca larga, você pode adicionar facilmente cubos de gelo e chá de frutas na garrafa, além de ser fácil de limpar.. O PRESENTE DE ESTILO DE VIDA PERFEITO: esta chaleira de qualidade superior é o presente perfeito para sua família, mãe, pai, marido, esposa, amante, vizinho, amigo ou colega."
+  },
+  {
+    "titulo": "Produto sem nome",
+    "imagem": "",
+    "preco": null,
+    "link": "",
+    "descricao": ""
   }
 ];
