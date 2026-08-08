bdc44dd152250176ea56d75e45a3e1c351895a6b
Mix Du Mix Ofertas <mix.dumixof@gmail.com>
2026-08-08 15:00:43 -0300

    Atualizacao automatica de precos: pedidos.js

--- pedidos.js
+++ pedidos.js
@@ -58,7 +58,7 @@ window.PEDIDOS = [
     "asin": "B093G99QBH",
     "titulo": "Fralda Pom Pom Protek Proteção de Mãe Jumbo XG 20 Unidades",
     "imagem": "https://m.media-amazon.com/images/I/61LQF0MSw3L._AC_SL1500_.jpg",
-    "preco": 28.41,
+    "preco": 28.43,
     "link": "https://www.amazon.com.br/dp/B093G99QBH?tag=mixstore04b3-20&ref_=mixdumix_pedido",
     "descricao": "Orelhas elásticas, garantindo mais conforto e um ajuste perfeito na cinturinha do bebê. Até 12 horas de absorção, garantindo dias e noites tranquilas. Não pesa, não cai - Canal superabsorvente, que distribui o xixi mais rápido, evitando que a fralda pese e caia entre as perninhas do bebê. Loção hidratante com extratos naturais que auxiliam na prevenção da irritação da pele. Cada tamanho tem um bichinho exclusivo da Fauna Brasileira. A embalagem pode variar."
   },
@@ -76,7 +76,7 @@ window.PEDIDOS = [
     "asin": "B07DYHDXX8",
     "titulo": "OMO Lavagem Perfeita - Sabão Líquido 5L",
     "imagem": "https://m.media-amazon.com/images/I/71bXBFl912L._AC_SL1500_.jpg",
-    "preco": 49.99,
+    "preco": 52.46,
     "link": "https://www.amazon.com.br/dp/B07DYHDXX8?tag=mixstore04b3-20&ref_=mixdumix_pedido",
     "descricao": "OMO Lavagem Perfeita deixa as suas roupas impecáveis, já que remove as manchas de primeira*. A fórmula do sabão líquido OMO possui perfume exclusivo que dura muito mais, isso tudo cuidando das cores e dos tecidos das suas roupas. O detergente OMO é imbatível na limpeza* e cuida do meio ambiente, pois sua fórmula contém ativos biodegradáveis. Este lava roupas cuida das cores e não deixa resíduos que danificam os tecidos, deixando suas roupas com cheirinho de limpeza. OMO Lavagem Perfeita cuida das cores sem danificar os tecidos e deixa suas roupas com cheirinho de limpeza"
   },
