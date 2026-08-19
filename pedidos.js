8d61b0d47ef8ea31bbf079588cd80de5fb2cae45
kcersarkal <kcersar.kal@gmail.com>
2026-08-19 14:09:27 -0300

    Novo pedido via bot

--- pedidos.js
+++ pedidos.js
@@ -1,3 +1,11 @@
 window.PEDIDOS = [
-  
+  {
+    "id": "B0FVP6KRP4",
+    "asin": "B0FVP6KRP4",
+    "titulo": "Electrolux Geladeira Electrolux Frost Free 320L Duplex Inox Look (TF38S) 127V",
+    "imagem": "https://m.media-amazon.com/images/I/416nfzkCKsL._AC_SL1000_.jpg",
+    "preco": null,
+    "link": "https://www.amazon.com.br/dp/B0FVP6KRP4?tag=mdm0a40-20",
+    "descricao": "A Geladeira. Electrolux. Frost. Duplex"
+  }
 ];
