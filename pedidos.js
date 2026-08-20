0e83195c0921a2b6c27721ae99e4e06b0265546e
kcersarkal <kcersar.kal@gmail.com>
2026-08-20 08:50:20 -0300

    Novo pedido via bot

--- pedidos.js
+++ pedidos.js
@@ -7,5 +7,14 @@ window.PEDIDOS = [
     "preco": null,
     "link": "https://www.amazon.com.br/dp/B0FVP6KRP4?tag=mdm0a40-20",
     "descricao": "A Geladeira. Electrolux. Frost. Duplex"
+  },
+  {
+    "id": "B0GYGFRXSY",
+    "asin": "B0GYGFRXSY",
+    "titulo": "Fritadeira Air Fryer Octo Fry Elgin – 8 Litros 1.800W 110V",
+    "imagem": "https://m.media-amazon.com/images/I/4186msR6oaL._AC_SL1000_.jpg",
+    "preco": 279.91,
+    "link": "https://www.amazon.com.br/dp/B0GYGFRXSY?tag=mdm0a40-20",
+    "descricao": "Capacidade de 8L. Se sua família é grande, sua Air Fryer também precisa ser.. A Octo Fry da Elgin chega para transformar sua rotina na cozinha, garantindo porções generosas para todos, seja nos almoços em família ou nos encontros com os amigos.. Com capacidade total de 8 litros e 1.800W de potência, é prática e econômica.. Sua tecnologia exclusiva, Air Circuit 360º, permite que o ar quente seja distribuído em espiral, resultando em refeições mais saudáveis, com até 80% menos gordura."
   }
 ];
