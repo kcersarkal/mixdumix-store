cfcd6b60636e9b8fca330298e0ca6eae204e937c
kcersarkal <kcersar.kal@gmail.com>
2026-08-21 07:32:50 -0300

    Novo pedido via bot

--- pedidos.js
+++ pedidos.js
@@ -16,5 +16,14 @@ window.PEDIDOS = [
     "preco": 279.91,
     "link": "https://www.amazon.com.br/dp/B0GYGFRXSY?tag=mdm0a40-20",
     "descricao": "Capacidade de 8L. Se sua família é grande, sua Air Fryer também precisa ser.. A Octo Fry da Elgin chega para transformar sua rotina na cozinha, garantindo porções generosas para todos, seja nos almoços em família ou nos encontros com os amigos.. Com capacidade total de 8 litros e 1.800W de potência, é prática e econômica.. Sua tecnologia exclusiva, Air Circuit 360º, permite que o ar quente seja distribuído em espiral, resultando em refeições mais saudáveis, com até 80% menos gordura."
+  },
+  {
+    "id": "B0FJMHMCBZ",
+    "asin": "B0FJMHMCBZ",
+    "titulo": "PHILIPS, Headphone Bluetooth, TAH2300BK/00, Com Microfone, Até 55 horas de bateria, Design Over-ear, Dobrável, Preto",
+    "imagem": "https://m.media-amazon.com/images/I/51BmQ8K9sML._AC_SL1000_.jpg",
+    "preco": 125.9,
+    "link": "https://www.amazon.com.br/dp/B0FJMHMCBZ?tag=mdm0a40-20",
+    "descricao": "MODO EXTRA BASS. Sinta toda a potência das suas músicas favoritas com o modo Extra Bass, que você ativa facilmente pressionando duas vezes o botão no fone. O isolamento passivo eficiente bloqueia ruídos externos, permitindo curtir cada batida com máxima intensidade.. 55 HORAS DE BATERIA. Curta por até 55 horas suas playlists completas sem precisar recarregar. E quando precisar, basta usar o cabo USB-C para recarregar e em apenas 2 horas você já tem a carga completa para curtir mais uma maratona musical.. SOM POTENTE COM ASSINATURA PHILIPS. Drivers grandes e especialmente ajustados oferecem áudio detalhado, graves profundos e equilíbrio sonoro, garantindo qualidade superior e a assinatura exclusiva Philips para uma experiência musical única e envolvente.. FÁCIL DE TRANSPORTAR. Leve seu headphone Philips para qualquer lugar com facilidade. O design dobrável deixa as conchas planas, facilitando o armazenamento em bolsas ou mochilas.. CONFORTO PROLONGADO COM DESIGN OVER-EAR. Com arco acolchoado, conchas ajustáveis e formato over-ear, o headphone garante maciez, ergonomia e isolamento acústico passivo, oferecendo conforto duradouro e redução natural de ruídos externos."
   }
 ];
